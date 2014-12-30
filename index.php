843d722a5e49c82f2d9019cc3efb835e55120388
Jessica Furtado <jessica@student.dei.uc.pt>
2014-12-30 01:02:05 +0000

    up antigo

--- index.php
+++ index.php
@@ -10,18 +10,18 @@
   <nav id="nav">
     <span class="level-one">
       <a class="nav-a"><img class="nav-a-img one" id="searchBtn" src="img/search.png"></a>
-      <span id="searchDiv" class="more-levels level-two" style="display:none; margin-left: 383px;">
-        <a class="nav-a two"><img class="nav-a-img two" id="roomBtn" src="img/cpu.png"></a>
-        <span id="roomDiv" class="more-levels level-three" style="display:none; margin-left: 0px;">
-          <a class="nav-a"><img class="nav-a-img three" id="roomBtnIn" src="img/cpu.png"></a>
-          <a class="nav-a"><img class="nav-a-img three" id="audBtn" src="img/drive.png"></a>
-          <a class="nav-a"><img class="nav-a-img three" id="labBtn" src="img/laptop.png"></a>
-        </span>
-        <a class="nav-a two"><img class="nav-a-img two" id="servBtn" src="img/motherboard.png"></a>
-        <a class="nav-a two"><img class="nav-a-img two" id="profBtn" src="img/mouse.png"></a>
-        <a class="nav-a two"><img class="nav-a-img two" id="eventsBtn" src="img/controller.png"></a>
-      </span>
       <a class="nav-a"><img class="nav-a-img one" id="quickBtn" src="img/cmd.png"></a>
+    </span>
+    <span id="searchDiv" class="more-levels level-two" style="display:none; margin-left: 383px;">
+      <a class="nav-a"><img class="nav-a-img two" id="roomBtn" src="img/cpu.png"></a>
+      <a class="nav-a"><img class="nav-a-img two" id="servBtn" src="img/motherboard.png"></a>
+      <a class="nav-a"><img class="nav-a-img two" id="profBtn" src="img/mouse.png"></a>
+      <a class="nav-a"><img class="nav-a-img two" id="eventsBtn" src="img/controller.png"></a>
+    </span>
+    <span id="roomDiv" class="more-levels level-three" style="display:none; margin-left: 510px;">
+      <a class="nav-a"><img class="nav-a-img three" id="roomBtnIn" src="img/cpu.png"></a>
+      <a class="nav-a"><img class="nav-a-img three" id="audBtn" src="img/drive.png"></a>
+      <a class="nav-a"><img class="nav-a-img three" id="labBtn" src="img/laptop.png"></a>
     </span>
   </nav>
 
@@ -30,17 +30,63 @@
   </div>
 
   <script>
-  $('#searchBtn').click(function(){
+  searchBtn = document.getElementById('searchBtn');
+  searchDiv = document.getElementById('searchDiv');
+  roomBtn = document.getElementById('roomBtn');
+  roomDiv = document.getElementById('roomDiv');
+  servBtn = document.getElementById('servBtn');
+  servDiv = document.getElementById('servDiv');
+
+  quickBtn = document.getElementById('quickBtn');
+
+  levelTwo = document.getElementsByClassName('level-two');
+  levelThree = document.getElementsByClassName('level-three');
+
+  $(searchBtn).click(function(){
+    if (searchDiv.style.display != 'none'){
+      $(quickBtn).animate({marginLeft: "0px"}, 200);
+      $(searchDiv).fadeOut(170);
+      $(levelThree).fadeOut(170);
+    } else {
+      $(quickBtn).animate({marginLeft: $(searchBtn).offset().left + "px"}, 200);
+      $(searchDiv).fadeIn(200);
+      searchDiv.style.display = "block";
+    }
+  });
+
+  $(roomBtn).click(function(){
+    if (roomDiv.style.display != 'none'){
+      $(servBtn).animate({marginLeft: "0px"}, 200);
+      $(roomDiv).fadeOut(170);
+    } else {
+      $(roomDiv).fadeIn(200);
+      roomDiv.style.display = "block";
+    }
+  });
+
+  $(servBtn).click(function(){
+    $(servBtn).css("background-color", "yellow");
+    if (roomDiv.style.display != 'none'){
+    //  $(servBtn).animate({marginLeft: "0px"}, 200);
+      $(roomDiv).fadeOut(170);
+    } else {
+    //  $(servBtn).css("margin-left", "-151px");
+    $(roomDiv).fadeIn(200);
+    $(levelTwo).not(servBtn).css("visibility", "hidden");
+    roomDiv.style.display = "block";
+    }
+  });
+
+/*  $('#searchBtn').click(function(){
     toggleOne('searchBtn','searchDiv', 'quickBtn');
   });
 
-/*  $('#quickBtn').click(function(){
-    toggleOne('quickBtn','searchDiv', 'quickBtn');
-  });*/
-
   $('#roomBtn').click(function(){
-    alert("cenas");
     toggleTwo('roomBtn', 'roomDiv', 'two', 'quickBtn');
+  });
+
+  $('#servBtn').click(function(){
+    toggleTwo('servBtn', 'roomDiv', 'two', 'quickBtn');
   });
 
   function toggleOne(id, id2, id3){
@@ -68,23 +114,20 @@
     var i3 = document.getElementsByClassName(id3);
     var i4 = document.getElementById(id4);
 
+    alert(iWidth);
+
     if (i2.style.display != 'none'){
-    //  $(i4).animate({marginLeft: "0px"}, 200);
-      $(i3).css("background-color","red");
-      $(i3).fadeIn(200);
-      $(i2).fadeOut(200);
-      //  $(i2).animate({marginLeft: iWidth+"px"}, 200);
+    $(i3).not(i).css("visibility", "visible");
+    $(i2).fadeOut(200);
     } else {
-    //  $(i4).animate({marginLeft: iWidth+7+"px"}, 200);
-    //  $(i3).fadeOut(170);
-    $(i3).css("background-color","white");
-    $(i2).fadeIn(200);
-      //  $(i2).animate({marginLeft: iWidth-132+"px"}, 200);
-      i2.style.display = "block";
+      alert(i);
+      $(i3).not(i).css("margin-left", "hidden");
+      $(i2).fadeIn(200);
+    i2.style.display = "block";
     }
   }
 
-
+*/
 /*
   function toggleTwo(id, id2, id3, id4){
     var i = document.getElementById(id);
